b7e2a763bbe5cd2ca02ee43ef1fa5e682c920f91
Hakan Yasak <hardwellhy@hotmail.com>
2021-10-24 20:16:23 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,31 @@
+console.log("Merhaba Kodlama.io")
+//10:20 değilll
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+dolarDun = "9.20"
+{
+let dolarDun = 9.10
+}
+
+
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11 //const ile değişken tanımladığımızda sadece bir kere tanımlanabilir yoksa hata verir//
+
+console.log(euroDun)
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Duygu Kredisi","Hakan Kredisi"] 
+
+console.log("<ul>")
+for(let i =0;i<konutKredileri.length;i++ ){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
+
+
+console.log(konutKredileri)
